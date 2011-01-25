b6524d122cbd22a93355e0835ecebf16c0f4e8cc
Alexandr <square_2@mail.ru>
2011-01-25 22:40:41 +0500

    cleaning bootloader

--- 3.bootloader/1.asm
+++ 3.bootloader/1.asm
@@ -5,15 +5,15 @@ _1:
 	mov ss,ax
 	mov sp,ax
 	sti
-_2:
+
 	mov es,ax
 	mov ax,0201h
 	mov cx,0002h
 	xor dx,dx
-	mov bx,buffer2
+	mov bx,sector2
 	int 13h
 	jc _error
-	mov bp,buffer2
+	mov bp,sector2
 	jmp _not_error
 _error:
 	mov bp,buffer1
@@ -25,10 +25,9 @@ _not_error:
 	cli
 	hlt
 	buffer1 db "fatal error",0h
-	buffer2 db "1-st sector"
-_3:
-	db 510 - (_3 -_1) dup(0)
+_2:
+	db 510 - (_2 -_1) dup(0)
 	db 55h, 0aah
 sector2 db "2-nd sector",0h
-_4:
-      db 512 -(_4 - sector2)  dup(0)
+_3:
+      db 512 -(_2 - sector2)  dup(0)
