993e5d5e773fbae5b18b1f03923ca19be6f0198e
Alexandr <square_2@mail.ru>
2011-01-25 17:19:18 +0500

    protected mode

---
+++ 4.protoss/1.asm
@@ -0,0 +1,76 @@
+org 07c00h
+_1:
+	mov ax,0h
+	mov ds,ax
+	mov es,ax
+
+	mov es,ax
+	mov ax,0204h
+	mov cx,0002h
+	xor dx,dx
+	mov bx,IDT
+	int 13h
+	jnc _not_error
+
+_error:
+	mov bp,buffer1
+	mov ax,1301h
+	mov bx,37h
+	mov cx,0bh
+	int 10h
+	cli
+	hlt
+	buffer1 db "fatal error",0h
+_not_error:
+	cli
+	lgdt [GDTpointer]
+	lidt [IDTpointer]
+	mov eax,cr0
+	or eax,1h
+	mov cr0,eax
+	sti
+	jmp 8:label0
+label0:
+	jmp 16:1234h
+	cli
+	hlt
+GDTpointer:
+	dw GDTlen - 1
+	dd GDT
+IDTpointer:
+	dw 8*256
+	dd IDT
+int13:
+	push ax
+	push es
+	mov ax,10h
+	mov es,ax
+	mov [es:0],word 'Hx'
+	mov [es:2],word 'ex'
+	mov [es:4],word 'lx'
+	mov [es:6],word 'lx'
+	mov [es:8],word 'ox'
+	pop es
+	pop ax
+	cli
+	hlt
+GDT:
+	db 8 dup(0)
+	db 0xff,0xff,0x00,0x00,0x00,10011000b,00001111b,0x00
+	db 0xff,0xff,0x00,0x80,0x0b,10010010b,00001111b,0x00
+GDTlen = $ - GDT
+
+_end1:
+	db 510 - (_end1 - _1) dup(0)
+db 55h,0aah
+IDT:
+	repeat 13
+		dd 2 dup(0)
+	end repeat
+	
+	dw	int13,8h
+	db	0h,10000111b
+	dw	0h
+	repeat 242
+	       dd 2 dup(0)
+	end repeat
