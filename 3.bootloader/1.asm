f89ddb930a9b4b7044ca43001636b25505a3dc5d
Alexandr <square_2@mail.ru>
2011-01-26 22:14:50 +0500

    additional work on 3rd task

--- 3.bootloader/1.asm
+++ 3.bootloader/1.asm
@@ -10,10 +10,10 @@ _1:
 	mov ax,0201h
 	mov cx,0002h
 	xor dx,dx
-	mov bx,sector2
+	mov bx,0x1230
 	int 13h
 	jc _error
-	mov bp,sector2
+	mov bp,0x1230
 	jmp _not_error
 _error:
 	mov bp,buffer1
@@ -26,8 +26,11 @@ _not_error:
 	hlt
 	buffer1 db "fatal error",0h
 _2:
-	db 510 - (_2 -_1) dup(0)
+	db 446 - (_2 -_1) dup(0)
+_partition_table:
+	db 64 dup(0)
+_mbr_signature: 
 	db 55h, 0aah
 sector2 db "2-nd sector",0h
 _3:
-      db 512 -(_2 - sector2)  dup(0)
+      db 512 -(_3 - sector2)  dup(0)
